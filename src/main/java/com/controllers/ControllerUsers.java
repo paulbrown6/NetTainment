00933d0a45adccfc18a79747aca183a86bfc702d
Paul Brown <paulbrown6@mail.ru>
2017-01-19 20:47:32 +0300

    Update 1.1

---
+++ src/main/java/com/controllers/ControllerUsers.java
@@ -0,0 +1,21 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Paul Brown on 19.01.2017.
+ */
+
+@Controller
+public class ControllerUsers {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView main() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
---
+++ src/main/webapp/pages/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Paul Brown
+  Date: 18.01.2017
+  Time: 10:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>NetTainment</title>
+  </head>
+<body>
+  <h1>Hello</h1>
+  <a href="${pageContext.request.contextPath}/pages/hews.jsp">Вход</a>
+</body>
+</html>
